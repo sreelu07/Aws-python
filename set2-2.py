93479d46d01c2b25ed24ba54d8054451713e4e80
sreelu07 <sreelu07@gmail.com>
2022-06-17 02:54:13 +0530

    all set2 files are solved

---
+++ set2-2.py
@@ -0,0 +1,14 @@
+
+"""2. With two given lists [1,3,6,78,35,55] and [12,24,35,24,88,120,155], write a
+program to make a list whose elements are intersection of the above given lists.
+
+"""
+list1= [1,3,6,78,35,55]
+list2=[12,24,35,24,88,120,155]
+set1=set(list1)
+set2=set(list2)
+a=set1.intersection(set2)
+b=list(a)
+print(b)
+
+
